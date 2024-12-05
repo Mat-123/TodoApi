b0c614e89ec493c07d1cc6e3d9aa3ee95f309284
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-12-05 18:16:09 +0100

    first get and post test

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e2205e61e5c17d478452c6456492c0548086a52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2147d8f9c97ca4a241b978146ae7ae77f872a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
